6cf4cae85865f2e0672daf2e8c9f7109e985beb1
sunguotao <sunguotao@jndl.mobi>
2017-12-06 16:45:36 +0800

    create project

---
+++ blog/static/images/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+'''
+
+@author: SunGuoTao
+
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+
+@contact: GuotaoSunVipSystem@gmail.com
+
+@software: garner
+
+@file: __init__.py.py
+
+@time: 2017/12/6 上午10:45
+
+@desc:
+
+'''
